46f06a31e96fca74cdd25d20b4d035878b32f25a
Alayna28 <alaynabaloch100@gmail.com>
2025-02-20 20:31:16 +0500

    Add files via upload

---
+++ .main.py
@@ -0,0 +1,8 @@
+# printing a greeting message
+print("hello guys!")
+# printing my name
+print("my name is alayna")
+# printing my hobbies
+print("I love to play badminton and like to read suspenseful books")
+# printing a math problem
+print("the result of 3.14 + 6.28 is:",3.14 + 6.28)
